cfdbdd3d53dc1a675db308dbd55bb607bda11a3f
yupeng <1196194875@qq.com>
2017-07-26 22:04:59 +0800

    update

--- schedule/index.js
+++ schedule/index.js
@@ -3,17 +3,17 @@ let service = require('./service/service');
 let rule  = new schedule.RecurrenceRule();
 let time = [];
 service.getQu();
-for(let i = 1;i<60;i++){
-    time.push(i);
-}
-rule.second = time;
-let c= 0;
-schedule.scheduleJob(rule,function(){
-    // c++;
-    console.log('定时执行',c);
-});
-console.log('pppp');
-setTimeout(function(){
-    c ='w';
-},5000);
-c = 'p';
+// for(let i = 1;i<60;i++){
+//     time.push(i);
+// }
+// rule.second = time;
+// let c= 0;
+// schedule.scheduleJob(rule,function(){
+//     // c++;
+//     console.log('定时执行',c);
+// });
+// console.log('pppp');
+// setTimeout(function(){
+//     c ='w';
+// },5000);
+// c = 'p';
--- schedule/service/service.js
+++ schedule/service/service.js
@@ -1,8 +1,9 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const https = require('https');
 const fs = require('fs');
 
-const url = 'https://nanjing.anjuke.com';
+const url = 'https://nanjing.anjuke.com/';
 
 let options = {
     url: url,
@@ -13,38 +14,34 @@ let options = {
 
 //获取区的列表
 let getQu = () => {
-    request(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            let $ = cheerio.load(body);
-            let list = $('.buy-house .clearfix .details .areas')[0].children;
-            let result = '';
-            for (let i = 0; i < list.length; i++) {
-                result = result + list[i].title + '=' + list[i].href + ';';
-            }
-            fs.writeFile('qu.txt', result, function (err) {
-                if (err) {
-                } else { }
-            })
-        } else {
-            let mockQu = {
-                "name1": "value1",
-                "name2": "value2",
-                "name3": "value3",
-                "name4": "value4",
-                "name5": "value5",
-                "name6": "value6",
-            };
-            mockQu = JSON.stringify(mockQu);
-            fs.writeFile('qu.txt', mockQu, function (err) {
-                if (err) {
-                    console.log('写入文件出错');
-                } else { }
-            })
-            console.log('获取区列表出错');
-            console.log(error);
-            console.log(response);
-        }
+    https.get(url,(res)=>{
+        console.log(res.statusCode);
+        res.on('data',(d)=>{
+            console.log(d);
+        })
     })
+    // request(options, function (error, response, body) {
+    //     if (!error && response.statusCode == 200) {
+    //         let $ = cheerio.load(body);
+    //         let list = $('.buy-house .clearfix .details .areas')[0] && $('.buy-house .clearfix .details .areas')[0].children;
+    //         let result = '';
+    //         for (let i = 0; list && i < list.length; i++) {
+    //             result = result + list[i].title + '=' + list[i].href + ';';
+    //         }
+    //         fs.writeFile('qu.txt', result, function (err) {
+    //             if (err) {
+    //             } else { }
+    //         })
+    //     } else {
+    //         console.log('获取区列表出错');
+    //         console.log(error);
+    //         fs.writeFile('response.txt', JSON.stringify(response), function (err) {
+    //             if (err) {
+    //                 console.log('写入文件出错');
+    //             } else { }
+    //         })
+    //     }
+    // })
 
 };
 
